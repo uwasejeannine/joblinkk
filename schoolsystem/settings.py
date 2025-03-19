8259511f3d3a1a54faec1c19778210d9fa26a262
unknown <uwasejeannine>
2025-03-19 12:12:03 -0800

    first commit

--- schoolsystem/settings.py
+++ schoolsystem/settings.py
@@ -97,6 +97,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Media files
 MEDIA_URL = '/images/'
